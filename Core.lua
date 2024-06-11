bfbfcf0c63b7a5705b6818a820309a6122cb85fa
Tyler Pentecost <tyler8806@gmail.com>
2024-06-10 23:06:56 -0500

    Added some test events.

--- Core.lua
+++ Core.lua
@@ -1,7 +1,9 @@
-local ZoleriiAddon = LibStub("AceAddon-3.0"):NewAddon("ZoleriiAddon", "AceConsole-3.0")
+local ZoleriiAddon = LibStub("AceAddon-3.0"):NewAddon("ZoleriiAddon", "AceConsole-3.0", "AceEvent-3.0")
 
 function ZoleriiAddon:OnInitialize()
     ZoleriiAddon:Print("OnInitialize")
+    ZoleriiAddon:RegisterChatCommand("zolerii", HandleChatCommand)
+    ZoleriiAddon:RegisterEvent("PLAYER_FOCUS_CHANGED", HandleFocusChange)
 end
 
 function ZoleriiAddon:OnEnable()
@@ -10,4 +12,12 @@ end
 
 function ZoleriiAddon:OnDisable()
     ZoleriiAddon:Print("OnDisable")
+end
+
+function HandleChatCommand()
+    ZoleriiAddon:Print("You used the chat command.")
+end
+
+function HandleFocusChange()
+    ZoleriiAddon:Print("Your focus changed.")
 end
